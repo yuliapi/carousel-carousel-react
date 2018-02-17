8862528a0eff6cc2093fda15508886aef75ad056
yulia <zEEcNCTDud0l>
2018-02-17 10:39:07 -0800

    Add info to readme.md.

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# carousel-carousel-react
+# Carousel-carousel-react
+Easy to use responsive slider component for React  designed for displaying images as animated slideshow.
+Tested in last versions of Chrome, Safari, Firefox and Opera 
+
+Check [live demo](http://yuliapi.github.io/projects/carousel/index.html?utm_source=github&utm_campaign=carousel)
+
+## How to use
+### Install 
+````
+npm install --save carousel-carousel-react
+```` 
+### Project structure
+```
+.
+|--src
+    |--images
+    |--App.js
+```
+### Prepare images
+To provide versions of an image for different display device scenarios this component use  HTML `<picture>` element in following way:
+
+````
+ <picture>
+     <source media="(min-width: 480px)" srcSet="image-large.jpg"/>
+     <img src="image-small.jpg" alt="image"/>
+ </picture>
+````
+In current version component follows mandatory naming convention that requires `'-size_suffix.ext'`. 
+We recommend can use following suffixes to name image files:
++ `image1_name-small.ext`
++ `image1_name-large.ext`
+
+or 
++ `image1_name-normal.ext` to use one image version for all screen sizes. As prefix use any valid file name.
+
+For instance, 
+- `monkey.ext` is invalid because of missing suffix;
+- `monkey-funny-size_suffix.ext` is valid;
+- `monkey-funny.ext` is valid to , but you'll get incorrect **alt** attribute (`alt="monkey"`)
+
+Supported formats .jpg, .png, .jpeg
+
+Place all images in image folder which should be in the same directory with App.js file
+
+### Usage
+
+In App.js import component:
+````
+import Carousel from 'carousel-carousel-react';
+```` 
+include following code:
+````
+function importAll(r) {
+    let images = {};
+    r.keys().map((item) => {
+        images[item.replace('./', '')] = r(item);
+        return null;
+    });
+    return images;
+}
+
+const images = importAll(require.context('./images', false, /\.(jpg)$/));
+````
+
+and use Carousel as:
+````
+ <Carousel images={images}/>
+```` 
+
--- build/components/Carousel.js
+++ build/components/Carousel.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -33,94 +33,102 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var Carousel = function (_Component) {
-    _inherits(Carousel, _Component);
+  _inherits(Carousel, _Component);
 
-    function Carousel(props) {
-        _classCallCheck(this, Carousel);
+  function Carousel(props) {
+    _classCallCheck(this, Carousel);
 
-        var _this = _possibleConstructorReturn(this, (Carousel.__proto__ || Object.getPrototypeOf(Carousel)).call(this, props));
+    var _this = _possibleConstructorReturn(this, (Carousel.__proto__ || Object.getPrototypeOf(Carousel)).call(this, props));
 
-        _this.changeSlide = function (currentActiveNumber) {
-            _this.setState({ currentActive: currentActiveNumber });
-        };
+    _this.changeSlide = function (currentActiveNumber) {
+      _this.setState({ currentActive: currentActiveNumber });
+    };
 
-        var images = Carousel.prepareImages(_this.props.images);
+    var images = Carousel.prepareImages(_this.props.images);
 
-        _this.state = {
-            id: 'myCarousel',
-            currentActive: 0,
-            images: images,
-            total: images.length
-        };
-        console.log(_this.state.image);
-        return _this;
+    _this.state = {
+      id: 'myCarousel',
+      currentActive: 0,
+      images: images,
+      total: images.length
+    };
+    return _this;
+  }
+
+  _createClass(Carousel, [{
+    key: 'render',
+    value: function render() {
+      var _this2 = this;
+
+      return _react2.default.createElement(
+        'div',
+        { id: this.state.id, className: 'carousel' },
+        _react2.default.createElement(
+          'div',
+          { className: 'carousel-body' },
+          this.state.images.map(function (image, index) {
+            return _react2.default.createElement(_Slide2.default, { key: index, index: index, title: image.alt,
+              small: image.small,
+              large: image.large,
+              currentSlide: _this2.state.currentActive });
+          })
+        ),
+        _react2.default.createElement(
+          'div',
+          { className: 'carousel-controls' },
+          _react2.default.createElement(_Control2.default, { direction: 'previous', controls: this.state.id,
+            totalSlides: this.state.total, currentSlide: this.state.currentActive,
+            handleClick: this.changeSlide }),
+          _react2.default.createElement(_Control2.default, { direction: 'next', controls: this.state.id,
+            totalSlides: this.state.total, currentSlide: this.state.currentActive,
+            handleClick: this.changeSlide })
+        ),
+        _react2.default.createElement(
+          'div',
+          { className: 'carousel-progress' },
+          _react2.default.createElement(_Progress2.default, { data: this.state, count: this.state.total,
+            handleClick: this.changeSlide })
+        )
+      );
     }
+  }], [{
+    key: 'prepareImages',
+    value: function prepareImages(images) {
+      var names = Object.keys(images);
+      var obj = {};
+      for (var i = 0; i < names.length; i++) {
+        var result = Carousel.extractPrefix(names[i]);
+        var prefix = result[1];
+        var s = { "alt": prefix };
+        s[result[2]] = images[names[i]];
 
-    _createClass(Carousel, [{
-        key: 'render',
-        value: function render() {
-            var _this2 = this;
-
-            return _react2.default.createElement(
-                'div',
-                { id: this.state.id, className: 'carousel' },
-                _react2.default.createElement(
-                    'div',
-                    { className: 'carousel-body' },
-                    this.state.images.map(function (image, index) {
-                        return _react2.default.createElement(_Slide2.default, { key: index, index: index, title: image.alt,
-                            small: image.small,
-                            large: image.large,
-                            currentSlide: _this2.state.currentActive });
-                    })
-                ),
-                _react2.default.createElement(
-                    'div',
-                    { className: 'carousel-controls' },
-                    _react2.default.createElement(_Control2.default, { direction: 'previous', controls: this.state.id,
-                        totalSlides: this.state.total, currentSlide: this.state.currentActive,
-                        handleClick: this.changeSlide }),
-                    _react2.default.createElement(_Control2.default, { direction: 'next', controls: this.state.id,
-                        totalSlides: this.state.total, currentSlide: this.state.currentActive,
-                        handleClick: this.changeSlide })
-                ),
-                _react2.default.createElement(
-                    'div',
-                    { className: 'carousel-progress' },
-                    _react2.default.createElement(_Progress2.default, { data: this.state, count: this.state.total,
-                        handleClick: this.changeSlide })
-                )
-            );
+        if (obj.hasOwnProperty(prefix) === true) {
+          var newEntry = Object.assign({}, obj[prefix], s);
+          obj[prefix] = newEntry;
+        } else {
+          obj[prefix] = s;
         }
-    }], [{
-        key: 'prepareImages',
-        value: function prepareImages(images) {
-            var names = Object.keys(images);
-            var obj = {};
-            for (var i = 0; i < names.length; i++) {
-                var result = Carousel.extractPrefix(names[i]);
-                var prefix = result[1];
-                var s = { "alt": prefix };
-                s[result[2]] = images[names[i]];
-
-                if (obj.hasOwnProperty(prefix) === true) {
-                    var newEntry = Object.assign({}, obj[prefix], s);
-                    obj[prefix] = newEntry;
-                } else {
-                    obj[prefix] = s;
-                }
-            }
-
-            return Object.values(obj);
+      }
+      var imageData = Object.values(obj);
+      imageData.map(function (entry) {
+        if (entry.hasOwnProperty('small') === false) {
+          entry.small = entry.large;
         }
-    }, {
-        key: 'extractPrefix',
-        value: function extractPrefix(imageName) {
-            return imageName.match(/(.*)-(\w+).(jpg|png|jpeg)$/);
+        if (entry.hasOwnProperty('large') === false) {
+          entry.large = entry.small;
         }
-    }]);
+      });
 
-    return Carousel;
+      return imageData;
+    }
+  }, {
+    key: 'extractPrefix',
+    value: function extractPrefix(imageName) {
+      return imageName.match(/(.*)-(\w+).(jpg|png|jpeg)$/);
+    }
+  }]);
+
+  return Carousel;
 }(_react.Component);
 
 exports.default = Carousel;
